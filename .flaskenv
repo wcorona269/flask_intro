e00b43233dda2778cc6c3c05a55741f591ed10b0
wcorona269 <wcorona269@gmail.com>
2023-03-15 16:24:42 -0400

    setup secret key

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=app
 FLASK_ENV=development
+SECRET_KEY=QrazBv6P5fojmc3xGttA1Z9qy68xRjP
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+print(app.config['SECRET_KEY'])
 
 @app.route('/')
 def index():
