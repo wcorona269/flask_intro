ac0005b9dbf9c2a701a1257318eb90ff1606034f
wcorona269 <wcorona269@gmail.com>
2023-03-15 17:37:37 -0400

    prepare jinja template setup

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,17 @@ def after_request_function(response):
 def splash():
   return '<h1>Hello World. Sample App.</h1>'
 
+@app.route('/help')
+def help():
+  return '<h1>Help page</h1>'
 
 @app.route('/about')
 def about():
   return '<h1>About</h1><p>My name is Will Corona. I am an App Academy graduate who is passionate about soccer and coding. I wanted to build an app to create easy access to find all of the latest news, scores, and updates for football/soccer all over the world</p>'
+
+@app.route('/item/<int:id>')
+def item(id):
+  if 0 < id and id < 100:
+    return f'''<h1>Item Number <h2>{id}</h2></h1>'''
+  else:
+    return '<h1>Item Not Found</h1>'
