a976f398261f332968681c107cfbf518aaffd397
wcorona269 <wcorona269@gmail.com>
2023-03-16 16:32:37 -0400

    setup nav and variable setting

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,17 @@ def splash():
 # def before_request_function():
 #   print('before request function running')
 
+@app.route('/item/<int:id>')
+def item(id):
+  if 0 < id and id < 100:
+    item = {
+      "id": id,
+      'name': f'fancy item {id}',
+      'description': 'Coming soon!'
+    }
+    return render_template('item.html', item=item)
+  else:
+    return '<h1>Item Not Found</h1>'
 
 # @app.after_request
 # def after_request_function(response):
@@ -18,17 +29,10 @@ def splash():
 #     return response
 
 
-# @app.route('/help')
-# def help():
-#   return '<h1>Help page</h1>'
+@app.route('/help')
+def help():
+  return '<h1></h1>'
 
 # @app.route('/about')
 # def about():
 #   return '<h1>About</h1><p>My name is Will Corona. I am an App Academy graduate who is passionate about soccer and coding. I wanted to build an app to create easy access to find all of the latest news, scores, and updates for football/soccer all over the world</p>'
-
-# @app.route('/item/<int:id>')
-# def item(id):
-#   if 0 < id and id < 100:
-#     return f'''<h1>Item Number <h2>{id}</h2></h1>'''
-#   else:
-#     return '<h1>Item Not Found</h1>'
