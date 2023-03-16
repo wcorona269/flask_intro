f9e2f8932fc39505debdcc3b659376e568201304
wcorona269 <wcorona269@gmail.com>
2023-03-16 14:38:15 -0400

    setup templates folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import (Flask, render_template)
 from app.config import Config
 
 app = Flask(__name__)
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,33 @@
 from app import app
 
-@app.before_request
-def before_request_function():
-  print('before request function running')
-
-
-@app.after_request
-def after_request_function(response):
-    print("after_request is running")
-    return response
 
 @app.route('/')
 @app.route('/splash')
 def splash():
-  return '<h1>Hello World. Sample App.</h1>'
+  return render_template('index.html')
 
-@app.route('/help')
-def help():
-  return '<h1>Help page</h1>'
+# @app.before_request
+# def before_request_function():
+#   print('before request function running')
 
-@app.route('/about')
-def about():
-  return '<h1>About</h1><p>My name is Will Corona. I am an App Academy graduate who is passionate about soccer and coding. I wanted to build an app to create easy access to find all of the latest news, scores, and updates for football/soccer all over the world</p>'
 
-@app.route('/item/<int:id>')
-def item(id):
-  if 0 < id and id < 100:
-    return f'''<h1>Item Number <h2>{id}</h2></h1>'''
-  else:
-    return '<h1>Item Not Found</h1>'
+# @app.after_request
+# def after_request_function(response):
+#     print("after_request is running")
+#     return response
+
+
+# @app.route('/help')
+# def help():
+#   return '<h1>Help page</h1>'
+
+# @app.route('/about')
+# def about():
+#   return '<h1>About</h1><p>My name is Will Corona. I am an App Academy graduate who is passionate about soccer and coding. I wanted to build an app to create easy access to find all of the latest news, scores, and updates for football/soccer all over the world</p>'
+
+# @app.route('/item/<int:id>')
+# def item(id):
+#   if 0 < id and id < 100:
+#     return f'''<h1>Item Number <h2>{id}</h2></h1>'''
+#   else:
+#     return '<h1>Item Not Found</h1>'
